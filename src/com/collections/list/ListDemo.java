f17bcc008eec7eab7dd706a4c7681fbe33eab843
anushkajain11 <anushka.jain1499@gmail.com>
2016-07-27 17:57:51 -0700

    Third commit

--- src/com/collections/list/ListDemo.java
+++ src/com/collections/list/ListDemo.java
@@ -1,5 +1,12 @@
 package com.collections.list;
 
 public class ListDemo {
+	
+	public static void main(String args[]){
+		//List<Integer> l1 = new ArrayList<Interger>();
+		
+		System.out.println("HI");
+	}
+	
 
 }
